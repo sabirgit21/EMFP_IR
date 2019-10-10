110d06350313357de555b97f6146212fa95fe490
OracleServer <OracleServer@DESKTOP-CF47LB6>
2019-10-10 11:33:40 +0500

    ro printingpage and class created

---
+++ ViewController/src/view/report/roprinting_report.java
@@ -0,0 +1,70 @@
+package view.report;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.faces.event.ValueChangeEvent;
+
+public class roprinting_report {
+    
+    private static String gotprinting = "";
+    
+    
+    public roprinting_report() {
+    }
+
+    public String gen_ro_printing_report() {
+        // Add event code here...
+        if (gotprinting == null) {
+            gotprinting = "notSelected";
+        }
+        OracleReportBean reportBean = new OracleReportBean("207.180.246.67", "9002", null);
+
+        //        reportBean.setReportURLName("userid=ir19/ir19@orcl&domain=classicdomain&report=C:/ERP/ir19/REPORTS/ReportsGl/SALE_INVOICE&");
+        String url = "";
+        reportBean.setReportParameter("app_det_id",gotprinting);
+        
+        switch (gotprinting) {
+        case "notSelected":
+        showMessage("Please Select Report Type");
+        break;
+        case "null":
+        showMessage("Please Select Report Type");
+        break;
+        default:
+//            System.out.println("phaseWise");
+//            reportBean.setReportURLName("userid=emfp/emfp@orcl&domain=classicdomain&report=C:/EMFP_Reports/Cluster_Detail_ase_Wise&");
+//            reportBean.setReportServerParam(OracleReportBean.RS_PARAM_DESTYPE,
+//                                            "CACHE"); // which will be one of the [cashe - file - mail - printer]
+//            reportBean.setReportServerParam(OracleReportBean.RS_PARAM_DESFORMAT,
+//                                            "PDF"); // Which will be onr of the [HTML - HTML CSS - PDF - SPREADSHEET- RTF].
+//            reportBean.setReportParameter("paramform", "no");
+//            
+//            url = reportBean.getReportServerURL();
+//            System.out.println("Url => " + url);
+//            reportBean.openUrlInNewWindow(url);
+            System.out.println("defaulter bla bla bla report");
+        break;
+        }
+            
+        return null;
+    }
+
+    public void get_ro_printing_name(ValueChangeEvent valueChangeEvent) {
+        // Add event code here...
+        gotprinting = (valueChangeEvent.getNewValue()).toString();
+        System.out.println("Selected Printing is : " + gotprinting);
+    }
+    
+    public String showMessage(String msgs) {
+        String messageText = msgs;
+        FacesMessage fm = new FacesMessage(messageText);
+        /**
+             * set the type of the message.
+             * Valid types: error, fatal,info,warning
+             */
+        fm.setSeverity(FacesMessage.SEVERITY_INFO);
+        FacesContext context = FacesContext.getCurrentInstance();
+        context.addMessage(null, fm);
+        return null;
+    }
+}
